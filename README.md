37ca9b6f4cb071692dbbc87c7a6e26585420936b
valerii_v <volkovskiy.v@gmail.com>
2024-02-20 20:47:36 +0300

    fix  types and move to es modules

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # notion-html-blocks
+
+`notion-html-blocks` is a zero-dependency library capable of rendering blocks from the Notion API into HTML.
+Through the use of the [Notion SDK for JavaScript](https://github.com/makenotion/notion-sdk-js) it's possible to
+extract specific page blocks which then can be converted into HTML content like this:
+
+```javascript
+import { renderBlocks } from 'notion-html-block'
+
+const pageBlocks = await notion.blocks.children.list({ block_id: id });
+
+renderBlocks(pageBlocks);
+```
+
+Example output for toggleable heading block:
+
+```html
+<h1 class="n-heading-1 n-toggleable n-toggleable-closed">Toggle Heading</h1>
+```
+
+### Motivation
+It`s  could be useful when you want to use Notion like CMS for your project
+
+### Supported types:
+* divider
+* code
+* image
+* bulleted_list_item
+* paragraph
+* heading_1
+* heading_2
+* heading_3
+* numbered_list_item
+* quote
+* toggle
+* callout
+* table
---
+++ tests/test.js
@@ -0,0 +1,133 @@
+import {renderBlockList} from '../src/index.js';
+
+const testData = [
+  {
+    object: "block",
+    id: "c56ba85c-4458-4b2e-a596-2bb42e2a0c3e",
+    parent: {
+      type: "page_id",
+      page_id: "5de27ba6-306a-446e-ad8c-3429474ee452",
+    },
+    created_time: "2024-02-11T22:24:00.000Z",
+    last_edited_time: "2024-02-19T20:51:00.000Z",
+    created_by: {
+      object: "user",
+      id: "93bfee87-3786-4846-8b2b-e66cc624a7e8",
+    },
+    last_edited_by: {
+      object: "user",
+      id: "93bfee87-3786-4846-8b2b-e66cc624a7e8",
+    },
+    has_children: false,
+    archived: false,
+    type: "paragraph",
+    paragraph: {
+      rich_text: [
+        {
+          type: "text",
+          text: { content: "Content is here", link: null },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: "Content is here",
+          href: null,
+        },
+      ],
+      color: "default",
+    },
+  },
+  {
+    object: "block",
+    id: "5629bbb9-54fb-41ad-9875-8715137e6877",
+    parent: {
+      type: "page_id",
+      page_id: "5de27ba6-306a-446e-ad8c-3429474ee452",
+    },
+    created_time: "2024-02-11T22:39:00.000Z",
+    last_edited_time: "2024-02-15T23:33:00.000Z",
+    created_by: {
+      object: "user",
+      id: "93bfee87-3786-4846-8b2b-e66cc624a7e8",
+    },
+    last_edited_by: {
+      object: "user",
+      id: "93bfee87-3786-4846-8b2b-e66cc624a7e8",
+    },
+    has_children: false,
+    archived: false,
+    type: "paragraph",
+    paragraph: {
+      rich_text: [
+        {
+          type: "text",
+          text: {
+            content: "This is my Article about ObserveR ",
+            link: null,
+          },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: "This is my Article about ObserveR ",
+          href: null,
+        },
+        {
+          type: "text",
+          text: { content: "Pattern ", link: null },
+          annotations: {
+            bold: true,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: "Pattern ",
+          href: null,
+        },
+        {
+          type: "text",
+          text: { content: "code", link: null },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: true,
+            color: "default",
+          },
+          plain_text: "code",
+          href: null,
+        },
+        {
+          type: "text",
+          text: { content: " variabe", link: null },
+          annotations: {
+            bold: false,
+            italic: false,
+            strikethrough: false,
+            underline: false,
+            code: false,
+            color: "default",
+          },
+          plain_text: " variabe",
+          href: null,
+        },
+      ],
+      color: "default",
+    },
+  },
+];
+
+const test = renderBlockList(testData);
+
+console.log(test);
